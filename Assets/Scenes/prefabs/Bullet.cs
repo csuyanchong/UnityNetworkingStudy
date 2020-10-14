5938f11a1b4491f83a0e2fa3b1434fe61d80be5d
yanchong1 <yanchong1@yiruikecorp.com>
2020-10-15 01:45:08 +0800

    UNet study. I refer to the website of "https://pmlpml.github.io/unity3d-learning/13-Multiplayer-and-Networking".

---
+++ Assets/Scenes/prefabs/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<PlayerMove>();
+
+        if (hitPlayer!= null)
+        {
+            print("Hit!");
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(30);
+
+            Destroy(gameObject);
+            
+        }
+    }
+}
---
+++ Assets/Scenes/prefabs/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerMove : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    float x,z;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+         x = Input.GetAxis("Horizontal") * 0.1f;
+         z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+        
+    }
+    [Command]
+    void CmdFire()
+    {
+        // create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward,
+            Quaternion.identity);
+
+        // make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        NetworkServer.Spawn(bullet);
+        // make bullet disappear after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
